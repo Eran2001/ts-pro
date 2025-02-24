0437ec318b4792b71a079e87584e94aa3b3ed8e2
Eran <eran.hasarali.2001@gmail.com>
2025-02-24 10:05:00 +0530

    test ts func

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import './style.css'
 
+myNew("Era")
+
+function myNew(name: string) {
+  console.log("Hello " + name);
+}
+
 const myFunc = (num1: number, num2: number) => {
   const total = num1 + num2
   console.log(`${num1} + ${num2} = ${total}`);
