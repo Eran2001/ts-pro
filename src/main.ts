b60ab9f75aba0e91035cd065c8a53560dc4454d2
Eran <eran.hasarali.2001@gmail.com>
2025-04-01 20:38:01 +0530

    remove some

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,1 @@
-import './style.css'
-
-const person: {name: string, age: number, isMarried: boolean} = {
-  name: "Era",
-  age: 23,
-  isMarried: false
-}
-
-console.log(person);
+import "./style.css";
